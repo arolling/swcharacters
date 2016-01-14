73ef39a64be26aaa57a7a28c20bbead7201962a9
Abigail Rolling and Eddie Duro <student@epicodus.com>
2016-01-14 15:26:45 -0800

    if/else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,41 @@
 $(document).ready(function() {
-  var count = 0;
   var total = 0;
+  var userInput;
 
-  for(var i = 1; i < 16; i ++) {
-    
-  }
+  $("form").submit(function(event) {
+    for(var i = 1; i < 6 ; i ++) {
+      var userInput = $("input[name=optionsRadios" + i + "]:checked").val();
+      userInput = parseInt(userInput);
+      total += userInput;
+      console.log(total);
+    }
+
+    if (total === 0) {
+      alert("jarjar");
+    } else if (total === 1) {
+      alert("vader");
+    } else if (total === 2) {
+      alert("kylo");
+    } else if (total <= 4) {
+      alert("obiwan");
+    } else if (total <= 6) {
+      alert("poe");
+    } else if (total === 7) {
+      alert("finn");
+    } else if (total <= 9) {
+      alert("rey");
+    } else if (total <= 11) {
+      alert("han");
+    } else if (total <= 13) {
+      alert("luke");
+    } else if (total <= 15) {
+      alert("leia");
+    }
+
+
+
+
+    event.preventDefault();
+  });
+
 });
